b09cc08ae675bb231bb4c99a6fb3b3370def3f0f
karravula chandra <94832445+karravulachandra@users.noreply.github.com>
2024-10-04 21:25:41 +0530

    Add files via upload

--- array_avg.java
+++ array_avg.java
@@ -1,21 +1,11 @@
-package Arrays;
-//import java.util.Arrays;
-import java.util.Scanner;
+package Package;
+
 public class array_avg {
-public static void main(String args[]) {
-	int a[]=new int[5];
-	int a1=0;
-	int avg;
-	for(int i=0;i<a.length;i++) {
-		try (Scanner sc = new Scanner(System.in)) {
-			a[i]=sc.nextInt();
-		//System.out.println("Hello");
-		a1=a1+a[i];
-		//System.out.println(Arrays.toString(a));
-					}
-	avg=a1/a.length;
-	System.out.println(avg);
-	
-}
-}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		array_avg sc=new array_avg();
+		
+	}
 }
